4ba2a29e4748709f894ff96935376b59789d42ab
Ethan <ethangchriss@gmail.com>
2025-03-12 21:32:24 -0700

    apostrophe fix

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -61,13 +61,13 @@ export default function Register() {
 
     // Validate form
     if (!passwordsMatch) {
-      setError("Passwords don't match");
+      setError("Passwords don&apos;t match");
       setIsLoading(false);
       return;
     }
 
     if (!passwordValid) {
-      setError("Your password doesn't meet the minimum requirements.");
+      setError("Your password doesn&apos;t meet the minimum requirements.");
       setIsLoading(false);
       return;
     }
@@ -216,7 +216,7 @@ export default function Register() {
                           >
                             <path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd" />
                           </svg>
-                          Passwords don't match
+                          Passwords don&apos;t match
                         </p>
                       )}
                     </div>
